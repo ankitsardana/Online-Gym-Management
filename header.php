ea98ff9962fd377cb398df1230bdf320f24a083a
Ankit Sardana <33087507+ankitsardana@users.noreply.github.com>
2018-07-21 13:33:59 +0530

    PHP File

---
+++ header.php
@@ -0,0 +1,22 @@
+
+		
+		
+				<header>
+				<div class="page">
+					<div class="header1"><a href="index1.html"><img src="logo111.gif" width="150" height="49" id="logo" alt="Fit-Club. Get trained professionals" /></a></div>
+					<div class="header2 center">
+						<img src="diamond.png" alt="" width="82" height="92" class="png" />
+		            </div>
+		            <div class="header3">
+					  <nav>
+		            	<a href="index1.html">Home</a>
+		            	<a href="about_us.html">About Us</a>
+		            	<a href="check.txt">Tips</a>
+		            	<a href="contact_us.html">Contact Us</a>
+		            	<a href="check.txt">FAQ</a>
+		              </nav>
+					</div>
+					</div>
+				</header>
+
+		
